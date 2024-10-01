a8c4fc8d07b999bd04e0e12217164fd3e2f38742
gus4214 <98795239+gus4214@users.noreply.github.com>
2024-10-01 14:55:28 +0900

    ✨ Mui 및 emotion ssr 세팅
    - Mui 패키지 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,38 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import Head from 'next/head';
+import { NextPage } from 'next';
+import type { AppProps } from 'next/app';
+import createEmotionCache from '@/src/createEmotionCache';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import { theme } from '@/src/@core/theme';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+interface MyAppProps extends AppProps {
+	emotionCache?: EmotionCache;
+	Component: NextPage;
 }
+
+const clientSideEmotionCache = createEmotionCache();
+
+const App = (props: MyAppProps) => {
+	const { emotionCache = clientSideEmotionCache, pageProps, Component } = props;
+
+	return (
+		<>
+			<CacheProvider value={emotionCache}>
+				<Head>
+					<meta charSet='utf-8' />
+					<meta name='viewport' content='width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1' />
+					<title>jipnadri</title>
+				</Head>
+
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<Component {...pageProps} />
+				</ThemeProvider>
+			</CacheProvider>
+		</>
+	);
+};
+
+export default App;
