8ba78f60bba32b19fcd16509ed7cdd9bad53a375
gus4214 <98795239+gus4214@users.noreply.github.com>
2024-10-05 01:32:20 +0900

    💄 상세페이지 이미지 확대 팝업 추가

--- src/pages/meetings/[id]/index.tsx
+++ src/pages/meetings/[id]/index.tsx
@@ -8,13 +8,28 @@ import InfoItem from '@/src/components/molecules/list/InfoItem';
 import UserItem from '@/src/components/molecules/list/UserItem';
 import DetailNavBar from '@/src/layouts/components/DetailNavBar';
 import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
+import CloseIcon from '@mui/icons-material/Close';
 import CorporateFareIcon from '@mui/icons-material/CorporateFare';
 import FmdGoodOutlinedIcon from '@mui/icons-material/FmdGoodOutlined';
-import { Box, Stack, Typography } from '@mui/material';
-import Image from 'next/image';
+import { Box, Dialog, DialogContent, Stack, Typography } from '@mui/material';
 import { NextPage } from 'next';
+import Image from 'next/image';
+import { useState } from 'react';
 
-const MeetingRecruitDetailPage: NextPage = () => {
+const MeetingDetailPage: NextPage = () => {
+	const [open, setOpen] = useState(false);
+	const [selectedImage, setSelectedImage] = useState<string>('');
+
+	const handleOpen = (imageSrc: string) => {
+		setSelectedImage(imageSrc);
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+		setSelectedImage('');
+	};
+
 	return (
 		<>
 			<DetailNavBar title='임장 모임 상세 정보' goBack />
@@ -41,8 +56,23 @@ const MeetingRecruitDetailPage: NextPage = () => {
 					<InfoItem icon={<CorporateFareIcon sx={{ width: '16px', height: '16px', color: 'grey.500', fontWeight: 600 }} />} label='1급지' />
 				</VerticalField>
 				<VerticalField label='임장 루트'>
-					<Box sx={{ position: 'relative', height: '212px', borderRadius: '8px', border: '1px solid var(--black-10, rgba(0, 0, 0, 0.10))' }}>
-						<Image src={'/images/sample/root-sample.jpeg'} alt='map' fill objectFit='contain' objectPosition='center' priority />
+					<Box
+						sx={{
+							position: 'relative',
+							height: '212px',
+							borderRadius: '8px',
+							border: '1px solid var(--black-10, rgba(0, 0, 0, 0.10))',
+							cursor: 'pointer',
+							':hover': {
+								'& .image-root': {
+									filter: 'brightness(90%)',
+									transition: '0.3s',
+								},
+							},
+						}}
+						onClick={() => handleOpen('/images/sample/root-sample.jpeg')} // 이미지 클릭 시 핸들러 호출
+					>
+						<Image className='image-root' src={'/images/sample/root-sample.jpeg'} alt='map' fill objectFit='cover' objectPosition='center' priority />
 					</Box>
 				</VerticalField>
 				<VerticalField label='세부 설명'>
@@ -54,8 +84,36 @@ const MeetingRecruitDetailPage: NextPage = () => {
 			<BottomFixedContainer bottom={60}>
 				<AppButton size='large'>모임 동행 요청하기</AppButton>
 			</BottomFixedContainer>
+
+			{/* 모달(Dialog) 컴포넌트 */}
+			<Dialog open={open} onClose={handleClose} maxWidth={'tablet'} fullScreen>
+				<DialogContent sx={{ position: 'relative', p: 0, width: '100%', height: '100svh' }}>
+					<Box
+						sx={{
+							width: '100%',
+							height: '100%',
+							position: 'relative',
+							backgroundColor: '#000',
+						}}
+					>
+						<Image src={selectedImage} alt='Full Size Image' fill objectFit='contain' objectPosition='center' priority />
+						<Box
+							aria-label='close'
+							onClick={handleClose}
+							sx={{
+								position: 'absolute',
+								cursor: 'pointer',
+								top: 8,
+								left: 8,
+							}}
+						>
+							<CloseIcon sx={{ color: 'white' }} fontSize='large' />
+						</Box>
+					</Box>
+				</DialogContent>
+			</Dialog>
 		</>
 	);
 };
 
-export default MeetingRecruitDetailPage;
+export default MeetingDetailPage;
